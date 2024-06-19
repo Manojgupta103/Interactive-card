dfa8393234cef75c6a8d60b0d573d63763431818
Manoj <manoj.gupta16539@sakec.ac.in>
2024-06-20 02:01:06 +0530

    chore: Refactor CSS classes and structure for image display and form layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,31 @@ import './App.css'
 
 function App() {
 
-
   return (
-    <div className='box'>
-    <div className='content'>
-    <div className='rdiv'>
-    <img src='./images/bg-main-desktop.png' alt='rimage'/>
-    </div>
-    <div className='ldiv'>
-    </div>
-    </div>
-      </div>
-  )
-}
+    <div className="container">
+      <div className="box">
+        <div className="image-section">
+          <img src="./images/bg-main-desktop.png" alt="Sample" />
+        </div>
+        <div className="color-section">
+          <div className='form'>
+            <h4>CARDHOLDER NAME</h4>
+            <input type="text" placeholder='e.g. James Jill'  />
 
+            <h4>CARD NUMBER</h4>
+            <input type="text" placeholder='e.g. 1234 5678 9123 0000'  />
+
+            <h4>EXP. DATE (MM/YY)</h4>
+            <input type="Number" placeholder='MM' style={{ width: '60px' }} />
+            <input type="Number" placeholder='YY' style={{ width: '60px' }} />
+
+            <h4>CVV</h4>
+            <input type="Number" placeholder='e.g. 123' style={{ width: '60px' }} />
+            <button>Confirm</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App
