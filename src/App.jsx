7fcd228eef2a3550c30fd89100fc06dd74bcb846
Manoj <manoj.gupta16539@sakec.ac.in>
2024-06-19 23:56:30 +0530

    chore: Add CSS classes and structure for image display

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
 
 
   return (
-    <>
-    
-       </>
+    <div className='box'>
+    <div className='content'>
+    <div className='rdiv'>
+    <img src='./images/bg-main-desktop.png' alt='rimage'/>
+    </div>
+    <div className='ldiv'>
+    </div>
+    </div>
+      </div>
   )
 }
 
